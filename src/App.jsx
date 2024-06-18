4713559ff20d7fbf3869fd3d33fa612a1b08bb68
mogomez967 <83328456+mogomez967@users.noreply.github.com>
2024-06-18 06:17:30 +0000

    added list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css'
 
-var my_list = [todoList];
+var todoList = [];
+
+function addTodo(id, title) {
+  const item = {id, title};
+  todoList.push(item);
+}
+
+addTodo(1, "Kilter Board");
+addTodo(2, "Muay Thai");
+addTodo(3, "Kickboxing");
 
 export default function App() {
   return (
     <main>
+      <h1>Todo List</h1>
+
+      <ul>
+        {
+          todoList.map(function(item) {
+            return <li key={item.id}>{item.title}</li>
+          })
+        }
+      </ul>
     </main>
   )
 }
